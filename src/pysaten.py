dfc00335a716f9db3eefcf93d24bb73762088439
Fumiyoshi MATANO <f_matano44@yahoo.co.jp>
2024-04-02 21:02:16 +0900

    mod: threshold (static -> dynamic)

--- src/pysaten.py
+++ src/pysaten.py
@@ -6,20 +6,21 @@ import numpy as np
 from numpy.random import default_rng
 import matplotlib.pyplot as plt
 from librosa.feature import zero_crossing_rate, rms
+import statistics as stat
 
 
 def vsed(data: np.ndarray, samplerate: float) -> tuple[float, float]:
-    _, _, start_s, end_s, _, _, _, _\
+    _, _, start_s, end_s, _, _, _, _, _\
         = _vsed_debug(data=data.copy(), samplerate=samplerate)
     return start_s, end_s
 
 
-def _vsed_debug(
+def vsed_debug(
         data: np.ndarray, samplerate: float,
         win_length_s:  float = None,
         hop_length_s:  float = 0.01,
-        rms_threshold: float = 0.01,
-        zcs_threshold: float = 0.5,
+        rms_threshold: float = None,
+        zcs_threshold: float = None,
         zcs_margin_s:  float = None,
         noise_seed:      int = 0
     ):
@@ -55,6 +56,7 @@ def _vsed_debug(
     x_nr_bpf = signal.filtfilt(band_b, band_a, x_nr)
     x_rms    = normalize(rms(
         y=x_nr_bpf, frame_length=win_length, hop_length=hop_length)[0])
+    rms_threshold = stat.mean(x_rms) if rms_threshold is None else rms_threshold
     rms_check = lambda x: rms_threshold < x
     start1: int = np.where(rms_check(x_rms))[0][0] \
         if np.any(rms_check(x_rms)) else 0
@@ -66,14 +68,15 @@ def _vsed_debug(
 
     # ZERO-CROSS
     x_nr_hpf = signal.filtfilt(high_b, [1.0], x_nr)
-    zcs = normalize(zero_crossing_rate(
+    x_zcs = normalize(zero_crossing_rate(
         x_nr_hpf, frame_length=win_length, hop_length=hop_length)[0])
+    zcs_threshold = stat.mean(x_zcs) if zcs_threshold is None else zcs_threshold
     ## start 側をスライド
     start2 = 0
     for i in range(_my_round(start1_s / hop_length_s), 0, -1):
-        if 0 < i and zcs_threshold <= zcs[i]:
+        if 0 < i and zcs_threshold <= x_zcs[i]:
             zcs_temp_start = max(0, i-int(zcs_margin/hop_length))
-            zcs_temp = zcs[zcs_temp_start: i]
+            zcs_temp = x_zcs[zcs_temp_start: i]
             zcs_idx = [j for j, z in enumerate(zcs_temp) if z <= zcs_threshold]
             if 0 < len(zcs_idx):
                 i -= len(zcs_temp) - max(zcs_idx)
@@ -81,10 +84,10 @@ def _vsed_debug(
                 start2 = i
                 break
     ## end 側をスライド
-    end2 = len(zcs)
-    for i in range(_my_round(end1_s / hop_length_s), len(zcs), 1):
-        if i < len(data) and zcs_threshold <= zcs[i]:
-            zcs_temp = zcs[i: i+int(zcs_margin/hop_length)]
+    end2 = len(x_zcs)
+    for i in range(_my_round(end1_s / hop_length_s), len(x_zcs), 1):
+        if i < len(data) and zcs_threshold <= x_zcs[i]:
+            zcs_temp = x_zcs[i: i+int(zcs_margin/hop_length)]
             j = [j for j, z in enumerate(zcs_temp) if z <= zcs_threshold]
             if j:  # is not Empty
                 i += min(j)
@@ -95,9 +98,9 @@ def _vsed_debug(
     start2_s = max(0                  , start2 * hop_length_s)
     end2_s   = min(end2 * hop_length_s, len(data) / samplerate)
 
-    temporal_positions = np.linspace(0, len(zcs) * hop_length_s, len(zcs))
+    feats_t = np.linspace(0, len(x_zcs) * hop_length_s, len(x_zcs))
     return start1_s, end1_s, start2_s, end2_s, \
-        temporal_positions, x_rms, zcs, zcs_threshold
+        feats_t, x_rms, rms_threshold, x_zcs, zcs_threshold
 
 
 def _add_noise_to_signal(signal, noise, desired_snr_db):
