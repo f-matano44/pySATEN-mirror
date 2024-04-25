6aab835ffd5bd01f60e19103cb49a8f05cffa479
Fumiyoshi MATANO <f_matano44@yahoo.co.jp>
2024-04-25 12:34:22 +0900

    mod: resample & blue noise

--- src/pysaten.py
+++ src/pysaten.py
@@ -5,50 +5,59 @@ from scipy import signal
 import numpy as np
 from numpy.random import default_rng
 import matplotlib.pyplot as plt
+from librosa import resample
 from librosa.feature import zero_crossing_rate, rms
 import statistics as stat
 
 
-def vsed(data: np.ndarray, samplerate: float) -> tuple[float, float]:
+def vsed(data: np.array, samplerate: float) -> tuple[float, float]:
     _, _, start_s, end_s, _, _, _, _, _\
         = vsed_debug(
             data=data.copy(),
             samplerate=samplerate,
-            rms_threshold=None,
-            zcs_threshold=None
+            rms_threshold=0.05,
+            zcs_threshold=0.5
         )
     return start_s, end_s
 
 
 def vsed_debug(
-        data: np.ndarray, samplerate: float,
+        data: np.array, samplerate: float,
         win_length_s:  float = None,
         hop_length_s:  float = 0.01,
         rms_threshold: float = None,
         zcs_threshold: float = None,
-        zcs_margin_s:  float = None,
+        margin_s:      float = None,
         noise_seed:      int = 0
     ):
     win_length_s = win_length_s if win_length_s is not None else hop_length_s * 4
-    zcs_margin_s = zcs_margin_s if zcs_margin_s is not None else win_length_s
+    zcs_margin_s = margin_s     if margin_s is not None     else win_length_s * 2
+
+    # resample
+    SATEN_FS     = 96000
+    if samplerate != SATEN_FS:
+        data = resample(y=data, orig_sr=samplerate, target_sr=SATEN_FS, res_type="soxr_lq")
+        samplerate   = SATEN_FS
 
     # constants
     nyq:        int = int(0.5 * samplerate)
-    f0_floor:   int = 71   # ref: WORLD (by Masanori MORISE)
-    f0_ceil:    int = 800  # ref: WORLD (by Masanori MORISE)
+    f0_floor:   int = 71   # from WORLD library
+    f0_ceil:    int = 800  # from WORLD library
     win_length: int = int(win_length_s * samplerate)
     hop_length: int = int(hop_length_s * samplerate)
-    zcs_margin: int = int(zcs_margin_s * samplerate) 
+    margin:     int = int(zcs_margin_s * samplerate) 
     band_b, band_a  = signal.butter(4, [f0_floor / nyq, f0_ceil / nyq], btype='band')
-    high_b          = signal.firwin(31, f0_floor / nyq, pass_zero=False)
+    high_b          = signal.firwin(31, f0_ceil / nyq, pass_zero=False)
     normalize       = lambda a: (a - a.min()) / (a.max() - a.min())
+    rand            = default_rng(noise_seed)
 
     # preprocess
-    ## add white noise
-    np_random = default_rng(noise_seed)
-    white_noise = np_random.normal(0, 1, len(data))
-    data = _add_noise_to_signal(data, white_noise, 20)
-    data = data if max(abs(data)) <= 1 else data / max(abs(data))
+    ## add blue noise
+    data_rms  = np.sort(rms(y=data)).flatten()
+    noise     = _gen_blue_noise(len(data), samplerate, rand)
+    noise_snr = min(20 * np.log10(data_rms[-2] / data_rms[1]), 10)
+    data      = _add_noise_to_signal(data, noise, noise_snr)
+    data      = data if max(abs(data)) <= 1 else data / max(abs(data))
     ## reduce start and end
     oneside_win_length = int(win_length / 2)
     data[: oneside_win_length] = \
@@ -62,11 +71,29 @@ def vsed_debug(
     x_rms    = normalize(rms(
         y=x_nr_bpf, frame_length=win_length, hop_length=hop_length)[0])
     rms_threshold = stat.mean(x_rms) if rms_threshold is None else rms_threshold
-    rms_check = lambda x: rms_threshold < x
-    start1: int = np.where(rms_check(x_rms))[0][0] \
-        if np.any(rms_check(x_rms)) else 0
-    end1:   int = np.where(rms_check(x_rms))[0][-1] \
-        if np.any(rms_check(x_rms)) else len(x_rms) - 1
+    ## start 側をスライド
+    start1 = 0
+    for i in range(len(x_rms)):
+        if 0 < i and rms_threshold <= x_rms[i]:
+            rms_temp_start = max(0, i-int(margin/hop_length))
+            rms_temp = x_rms[rms_temp_start: i]
+            rms_idx = [j for j, z in enumerate(rms_temp) if z <= rms_threshold]
+            if 0 < len(rms_idx):
+                i -= len(rms_temp) - max(rms_idx)
+            else:
+                start1 = i
+                break
+    ## end 側をスライド
+    end1 = len(x_rms)
+    for i in range(len(x_rms)-1, 0, -1):
+        if i < len(data) and rms_threshold <= x_rms[i]:
+            rms_temp = x_rms[i: i+int(margin/hop_length)]
+            j = [j for j, z in enumerate(rms_temp) if z <= rms_threshold]
+            if j:  # is not Empty
+                i += min(j)
+            else:
+                end1 = i
+                break
 
     start1_s = max(0,                   start1 * hop_length_s)
     end1_s   = min(end1 * hop_length_s, len(data) / samplerate)
@@ -80,7 +107,7 @@ def vsed_debug(
     start2 = 0
     for i in range(_my_round(start1_s / hop_length_s), 0, -1):
         if 0 < i and zcs_threshold <= x_zcs[i]:
-            zcs_temp_start = max(0, i-int(zcs_margin/hop_length))
+            zcs_temp_start = max(0, i-int(margin/hop_length))
             zcs_temp = x_zcs[zcs_temp_start: i]
             zcs_idx = [j for j, z in enumerate(zcs_temp) if z <= zcs_threshold]
             if 0 < len(zcs_idx):
@@ -92,7 +119,7 @@ def vsed_debug(
     end2 = len(x_zcs)
     for i in range(_my_round(end1_s / hop_length_s), len(x_zcs), 1):
         if i < len(data) and zcs_threshold <= x_zcs[i]:
-            zcs_temp = x_zcs[i: i+int(zcs_margin/hop_length)]
+            zcs_temp = x_zcs[i: i+int(margin/hop_length)]
             j = [j for j, z in enumerate(zcs_temp) if z <= zcs_threshold]
             if j:  # is not Empty
                 i += min(j)
@@ -108,8 +135,27 @@ def vsed_debug(
         feats_timestamp, x_rms, rms_threshold, x_zcs, zcs_threshold
 
 
+def _gen_blue_noise(
+        length: int, fs: int, rand: np.random.default_rng
+    ) -> np.array:
+
+    length2 = length + 1000
+    # white noise
+    wh = rand.uniform(low=-1.0, high=1.0, size=length2)
+    # fft
+    WH = np.fft.rfft(wh)
+    WH_f = np.fft.rfftfreq(len(wh), 1/fs)
+    # white -> blue
+    BL = WH * np.sqrt(WH_f)
+    # ifft
+    bl = np.fft.irfft(BL)
+    # normalize
+    bl /= np.max(np.abs(bl))
+
+    return bl[: length]
+
+
 def _add_noise_to_signal(signal, noise, desired_snr_db):
-    """ 指定されたSNRでノイズを信号に追加する """
     desired_snr_linear = 10 ** (desired_snr_db / 10)
     signal_power = np.mean(signal ** 2)
     noise_power = np.mean(noise ** 2)
