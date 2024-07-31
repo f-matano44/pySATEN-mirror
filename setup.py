5641b3d576b9b7f7f3e058014cce627b68e8911b
Fumiyoshi MATANO <f_matano44@yahoo.co.jp>
2024-07-31 17:08:16 +0900

    add: dependancy

--- setup.py
+++ setup.py
@@ -1,3 +1,8 @@
 from setuptools import find_packages, setup
 
-setup(name="pysaten", version="1.0", packages=find_packages())
+setup(
+    name="pysaten",
+    version="1.0",
+    packages=find_packages(),
+    install_requires=["numpy", "noisereduce", "librosa", "scipy"],
+)
