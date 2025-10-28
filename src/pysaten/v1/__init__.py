e74388ccb45ea3cec44a4958a16b8c7986198559
Fumiyoshi MATANO <fumiyoshi.matano@gmail.com>
2025-10-28 12:31:45 +0900

    mod annotation for v1

--- src/pysaten/v1/__init__.py
+++ src/pysaten/v1/__init__.py
@@ -4,8 +4,8 @@ from typing import Final, Optional
 
 import noisereduce as nr
 import numpy as np
-import numpy.typing as npt
 from librosa import resample
+from numpy.typing import NDArray
 from scipy.signal import cheby1, firwin, lfilter, sosfilt
 
 from ..utility import color_noise
@@ -17,7 +17,7 @@ from ..utility.signal import zero_crossing_rate as zcr
 
 
 def vsed_debug_v1(
-    y: npt.NDArray,
+    y: NDArray[float],
     orig_sr: float,
     # -------------------------------------------
     win_length_s: Optional[float] = None,
@@ -29,9 +29,19 @@ def vsed_debug_v1(
     offset_s: float = 0.03,
     # -------------------------------------------
     noise_seed: int = 0,
-):
+) -> tuple[
+    float,
+    float,
+    float,
+    float,
+    float,
+    float,
+    NDArray[float],
+    NDArray[float],
+    NDArray[float],
+]:
     # resample
-    y_rsp: Final[npt.NDArray] = resample(
+    y_rsp: Final[NDArray[float]] = resample(
         y=y, orig_sr=orig_sr, target_sr=SR, res_type="soxr_lq"
     )
 
@@ -42,7 +52,7 @@ def vsed_debug_v1(
     zcr_margin: Final[int] = int(zcr_margin_s / hop_length_s)
 
     # preprocess: add blue noise && remove background noise
-    y_nr: Final[npt.NDArray] = _00_preprocess(y_rsp, SR, noise_seed)
+    y_nr: Final[NDArray[float]] = _00_preprocess(y_rsp, SR, noise_seed)
 
     # step1: Root mean square
     start1, end1, y_rms = _01_rms(y_nr, SR, rms_threshold, win_length, hop_length)
@@ -71,7 +81,7 @@ def vsed_debug_v1(
     start3_s: Final[float] = max(0, start2_s - offset_s)
     end3_s: Final[float] = min(end2_s + offset_s, len(y_rsp) / SR)
 
-    feats_timestamp: Final[npt.NDArray] = np.linspace(
+    feats_timestamp: Final[NDArray[float]] = np.linspace(
         0, len(y_zcr) * hop_length_s, len(y_zcr)
     )
 
@@ -88,26 +98,26 @@ def vsed_debug_v1(
     )
 
 
-def _00_preprocess(y: npt.NDArray, sr: int, noise_seed: int) -> npt.NDArray:
-    data_rms: Final[npt.NDArray] = np.sort(rms(y, 2048, 512))  # <- default of librosa
+def _00_preprocess(y: NDArray[float], sr: int, noise_seed: int) -> NDArray[float]:
+    data_rms: Final[NDArray[float]] = np.sort(rms(y, 2048, 512))  # <- default of librosa
     signal_amp: Final[float] = data_rms[-2]
     noise_amp: Final[float] = max(data_rms[1], 1e-10)
     snr: Final[float] = min(20 * np.log10(signal_amp / noise_amp), 10)
-    noise: Final[npt.NDArray] = color_noise.blue(len(y), sr, noise_seed)
-    y_blue: Final[npt.NDArray] = y + noise * (signal_amp / 10 ** (snr / 20))
-    y_blue_normalized: Final[npt.NDArray] = (
+    noise: Final[NDArray[float]] = color_noise.blue(len(y), sr, noise_seed)
+    y_blue: Final[NDArray[float]] = y + noise * (signal_amp / 10 ** (snr / 20))
+    y_blue_normalized: Final[NDArray[float]] = (
         y_blue if np.max(np.abs(y_blue)) <= 1 else y_blue / np.max(np.abs(y_blue))
     )
     return nr.reduce_noise(y_blue_normalized, sr)
 
 
 def _01_rms(
-    y: npt.NDArray, sr: int, threshold: float, win_length: int, hop_length: int
-) -> tuple[int, int, npt.NDArray]:
+    y: NDArray[float], sr: int, threshold: float, win_length: int, hop_length: int
+) -> tuple[int, int, NDArray[float]]:
     wp: Final[tuple[float, float]] = (F0_FLOOR / NYQ, F0_CEIL / NYQ)
-    band_sos: Final[npt.NDArray] = cheby1(12, 1, wp, "bandpass", output="sos")
-    y_bpf: Final[npt.NDArray] = sosfilt(band_sos, y)
-    y_rms: Final[npt.NDArray] = normalize(rms(y_bpf, win_length, hop_length))
+    band_sos: Final[NDArray[float]] = cheby1(12, 1, wp, "bandpass", output="sos")
+    y_bpf: Final[NDArray[float]] = sosfilt(band_sos, y)
+    y_rms: Final[NDArray[float]] = normalize(rms(y_bpf, win_length, hop_length))
     start1: Final[int] = (
         np.where(threshold < y_rms)[0][0] if np.any(threshold < y_rms) else 0
     )
@@ -120,7 +130,7 @@ def _01_rms(
 
 
 def _02_zcr(
-    y: npt.NDArray,
+    y: NDArray[float],
     sr: int,
     start1: int,
     end1: int,
@@ -128,10 +138,10 @@ def _02_zcr(
     margin: int,
     win_length: int,
     hop_length: int,
-) -> tuple[int, int, npt.NDArray]:
+) -> tuple[int, int, NDArray[float]]:
     high_b: Final[float] = firwin(101, F0_CEIL, pass_zero=False, fs=sr)
-    y_hpf: Final[npt.NDArray] = lfilter(high_b, 1.0, y)
-    y_zcr: Final[npt.NDArray] = normalize(zcr(y_hpf, win_length, hop_length))
+    y_hpf: Final[NDArray[float]] = lfilter(high_b, 1.0, y)
+    y_zcr: Final[NDArray[float]] = normalize(zcr(y_hpf, win_length, hop_length))
     # slide start index
     start2: Final[int] = slide_index(
         goto_min=True,
